8487a4d001aa20d880b17aedfb5bf4ea1a318df3
DanielLastre <daniellastre02@gmail.com>
2026-05-29 20:22:07 -0500

    ssq

--- TaskFlowSPA/src/main.js
+++ TaskFlowSPA/src/main.js
@@ -1,1 +1,9 @@
 import "./styles/global.css";
+import { renderLogin } from "./views/auth/login";
+import { renderRegister } from "./views/auth/register";
+import { renderHome } from "./views/users/home";
+
+const app = document.getElementById("app");
+
+app.innerHTML = renderHome();
+app.innerHTML = renderRegister();
